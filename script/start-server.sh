aa7b6ce81509b6d62f50c6db22f2f687ddd929e7
Don Brower <dbrower@nd.edu>
2014-11-25 14:50:52 -0500

    Add unicorn stuff

---
+++ script/start-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Starts the server on the dl-vecnet server. It assumes
+# there is an nginx reverse proxy at port 80
+
+source /etc/profile.d/chruby.sh
+chruby 2.0.0-p353
+
+#LISTEN_PORTS="-l 127.0.0.1:3001"
+app_root=$(cd $(dirname $0)/.. && pwd)
+source $app_root/script/get-env.sh
+cd $RAILS_ROOT
+bundle exec unicorn -D -E deployment -c $RAILS_ROOT/config/unicorn.rb
